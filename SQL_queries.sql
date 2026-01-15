88be48ba5d0acce11eb477734a4309e54cb63221
Muhammad Saad <jawadawan4203@gmail.com>
2026-01-15 22:29:15 +0500

    Update SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -75,6 +75,8 @@ FROM
     product_category_name_translation AS translation ON data.name = translation.ï»¿product_category_name
 ORDER BY no_orders DESC;
 
+#-------
+
 #Q5.b: Top 10 categories by Sales
 SELECT 
     translation.product_category_name_english AS name_of_category,
@@ -92,6 +94,8 @@ FROM
         JOIN
     product_category_name_translation AS translation ON data.name = translation.ï»¿product_category_name
 ORDER BY sales DESC;
+
+#-------
 
 #Q5.c: Top 10 categories by No of orders reviews
 SELECT 
@@ -113,6 +117,8 @@ FROM
         JOIN
     product_category_name_translation AS translation ON data.name = translation.ï»¿product_category_name
 ORDER BY no_of_orders DESC;
+
+#-------
 
 #Q6: Top 10 sellers by No of orders with cities, states, revenue, avg rating and ranking according to no_orders
 SELECT 
@@ -137,6 +143,8 @@ HAVING sellers.seller_id IS NOT NULL
 ORDER BY no_of_orders DESC
 LIMIT 10;
 
+#-------
+
 #Q7: Which payment methode is most used with revenue
 SELECT 
     payment_type,
@@ -147,6 +155,8 @@ FROM
 GROUP BY payment_type
 HAVING payment_type <> 'not_defined';
 
+#-------
+
 #Q8: More revenue from installment or 1-time pay
 SELECT 
     ROUND(SUM(CASE WHEN payment_installments <> '1' THEN payment_value ELSE 0 END), 0) AS installment_total,
@@ -154,6 +164,8 @@ SELECT
     ROUND(SUM(CASE WHEN payment_installments <> '1' THEN payment_value ELSE 0 END) - 
           SUM(CASE WHEN payment_installments = '1' THEN payment_value ELSE 0 END), 0) AS difference
 FROM olist_order_payments_dataset;
+
+#-------
 
 #Q9: Fast vs Late vs on-time Delivery
 SELECT 
@@ -163,6 +175,8 @@ SELECT
 FROM olist_orders_dataset
 WHERE order_status = 'delivered' 
   AND order_delivered_customer_date IS NOT NULL;
+  
+#-------  
   
 #Q10: avg delvery time from order book to delivered by cities
 SELECT 
@@ -180,6 +194,8 @@ WHERE
 GROUP BY cities
 ORDER BY no_of_orders DESC;
 
+#-------
+
 #Q11: No of orders by years and months 
 SELECT 
     DATE_FORMAT(order_purchase_timestamp, '%Y') AS year,
@@ -192,7 +208,40 @@ WHERE
 GROUP BY year , month , MONTH(order_purchase_timestamp)
 ORDER BY year , MONTH(order_purchase_timestamp);
 
+#-------
+
 #Q12: Different order status
-select order_status, count( order_id) as no_of_orders from olist_orders_dataset
-group by order_status
-order by no_of_orders desc;
+SELECT 
+    order_status, COUNT(order_id) AS no_of_orders
+FROM
+    olist_orders_dataset
+GROUP BY order_status
+ORDER BY no_of_orders DESC;
+
+#-------
+
+#Q13: Repeat vs one-time 
+SELECT 
+    SUM(CASE WHEN order_count > 1 THEN 1 ELSE 0 END) AS repeat_customers,
+    SUM(CASE WHEN order_count = 1 THEN 1 ELSE 0 END) AS one_time_customers
+FROM (
+    SELECT 
+        customer_unique_id, 
+        COUNT(order_id) AS order_count
+    FROM olist_orders_dataset AS orders
+    JOIN olist_customers_dataset AS customers ON orders.customer_id = customers.customer_id
+    GROUP BY customer_unique_id
+) AS customer_orders;
+
+#-------
+
+#Q14: Delivery vs Rating
+SELECT 
+  CASE 
+    WHEN DATEDIFF(order_estimated_delivery_date,order_delivered_customer_date)<0 
+    THEN 'Late' ELSE 'On/Fast' END AS delivery_status,
+  ROUND(AVG(review_score),2) AS avg_rating
+FROM olist_orders_dataset o
+JOIN olist_order_reviews_dataset r ON o.order_id=r.order_id
+WHERE order_status='delivered'
+GROUP BY delivery_status;
